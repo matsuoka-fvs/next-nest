f8c2792470077ce58167d3a56b72a6de4a76e70b
松岡利樹 <matsuoka@fvs-net.co.jp>
2022-07-21 17:45:38 +0900

    ユーザー登録機能完了

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BikesModule } from './bikes/bikes.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     BikesModule,
     TypeOrmModule.forRoot(),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
